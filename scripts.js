6ce670f1127e14b5670fb8355008895a1d7f3d50
J-Skarbek <john@snarkmediaagency.com>
2022-11-06 11:14:41 -0600

    Add basic conditional check for validity.

--- scripts.js
+++ scripts.js
@@ -5,3 +5,12 @@ const country = document.getElementById('country')
 const zipCode = document.getElementById('zip-code')
 const password = document.getElementById('password')
 const verifyPassword = document.getElementById('verify-password')
+const submitBtn = document.getElementById('submit')
+
+const checkValidity = () => {
+  if ((!firstName.checkValidity()) || (!lastName.checkValidity()) || (!email.checkValidity()) || (!country.checkValidity()) || (!zipCode.checkValidity()) || (!password.checkValidity()) || (!verifyPassword.checkValidity())) {
+    console.log('there are problems here.')
+  }
+}
+
+submitBtn.addEventListener('click', checkValidity)
