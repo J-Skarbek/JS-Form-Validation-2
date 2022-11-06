2519f64876309331d9c3a13830bc728a8e87f71b
J-Skarbek <john@snarkmediaagency.com>
2022-11-06 11:42:55 -0600

    Add more validation to the html elements and print an error code to the DOM.

--- scripts.js
+++ scripts.js
@@ -6,10 +6,12 @@ const zipCode = document.getElementById('zip-code')
 const password = document.getElementById('password')
 const verifyPassword = document.getElementById('verify-password')
 const submitBtn = document.getElementById('submit')
+const errorDisplay = document.querySelector('.error-display p')
 
 const checkValidity = () => {
   if ((!firstName.checkValidity()) || (!lastName.checkValidity()) || (!email.checkValidity()) || (!country.checkValidity()) || (!zipCode.checkValidity()) || (!password.checkValidity()) || (!verifyPassword.checkValidity())) {
     console.log('there are problems here.')
+    errorDisplay.textContent = 'Problems have been detected.'
   }
 }
 
